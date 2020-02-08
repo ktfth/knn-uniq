b0be42c2d99530a59aceafea9a636a7af3a2978f
Kaique da Silva <kaeyosthaeron@gmail.com>
2020-02-08 16:30:19 -0300

    Project started

---
+++ index.js
@@ -0,0 +1,23 @@
+const assert = require('assert');
+const knn = require('knn');
+
+const uniq = (a, b) => knn.neighbor(a, b) > 0;
+assert.ok(!uniq({a: 1, b: 2}, { a: 1, b: 2}));
+const notUniq = (a, b) => knn.neighbor(a, b) === 0;
+assert.ok(notUniq({a: 1, b: 2}, {a: 1, b: 2}));
+const deepUniq = (arr) => {
+    let out = [];
+    arr.map(item => {
+        if (out.length === 0) {
+            out.push(item);
+        } else {
+            out.map(subItem => {
+                if (uniq(item, subItem)) {
+                    out.push(item);
+                }
+            });
+        }
+    });
+    return out;
+};
+assert.deepEqual(deepUniq([{a: 1, b: 2}, {a: 1, b: 2}, {c: 3, d: 4}]), [{a: 1, b: 2}, {c: 3, d: 4}]);
