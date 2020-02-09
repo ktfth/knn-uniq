76cbe18e49474ea7b61316c9e8012d4795f5397c
Kaique da Silva <kaeyosthaeron@gmail.com>
2020-02-09 18:33:42 -0300

    Full featured method

--- index.js
+++ index.js
@@ -13,14 +13,23 @@ const deepUniq = root.deepUniq = (arr) => {
     arr.map(item => {
         if (out.length === 0) {
             out.push(item);
-        } else if (out.filter(internal => uniq(item, internal)).length > 0) {
+        } if (out.filter(internal => uniq(item, internal)).length > 0) {
             out.push(item);
+        } else {
+            out.map(subItem => {
+                if (uniq(item, subItem)) {
+                    out.push(item);
+                }
+            });
         }
     });
     return out;
 };
 assert.deepEqual(deepUniq([{a: 1, b: 2}, {a: 1, b: 2}, {c: 3, d: 4}]), [{a: 1, b: 2}, {c: 3, d: 4}]);
 assert.deepEqual(deepUniq([{a: 1}, {a: 1}, {b: 2, c: 3}, {d: 4, e: 5}]), [{a: 1}, {b: 2, c: 3}, {d: 4, e: 5}]);
+assert.deepEqual(deepUniq(deepUniq([{a: 1}, {b: 2, c: 3}, {b: 2, c: 3}]).reverse()).reverse(), [{a: 1}, {b: 2, c: 3}]);
+const neighborUniq = root.neighborUniq = (arr) => deepUniq(deepUniq(arr).reverse()).reverse();
+assert.deepEqual(neighborUniq([{a: 1}, {b: 2, c: 3}, {b: 2, c: 3}]), [{a: 1}, {b: 2, c: 3}]);
 
 const extend = root.extend = (destination, source) => {
     for (var property in source)
