367011bc3ab661e42a9ea2fe053e54afbf143a42
Kaique da Silva <kaeyosthaeron@gmail.com>
2020-02-10 14:05:12 -0300

    Working on the middle of array unique values

--- index.js
+++ index.js
@@ -11,9 +11,7 @@ assert.ok(notUniq({a: 1, b: 2}, {a: 1, b: 2}));
 const deepUniq = root.deepUniq = (arr) => {
     let out = [];
     arr.map(item => {
-        if (out.length === 0) {
-            out.push(item);
-        } if (out.filter(internal => uniq(item, internal)).length > 0) {
+        if (out.length === 0 || out.filter(internal => uniq(item, internal)).length > 0) {
             out.push(item);
         } else {
             out.map(subItem => {
@@ -28,7 +26,22 @@ const deepUniq = root.deepUniq = (arr) => {
 assert.deepEqual(deepUniq([{a: 1, b: 2}, {a: 1, b: 2}, {c: 3, d: 4}]), [{a: 1, b: 2}, {c: 3, d: 4}]);
 assert.deepEqual(deepUniq([{a: 1}, {a: 1}, {b: 2, c: 3}, {d: 4, e: 5}]), [{a: 1}, {b: 2, c: 3}, {d: 4, e: 5}]);
 assert.deepEqual(deepUniq(deepUniq([{a: 1}, {b: 2, c: 3}, {b: 2, c: 3}]).reverse()).reverse(), [{a: 1}, {b: 2, c: 3}]);
-const neighborUniq = root.neighborUniq = (arr) => deepUniq(deepUniq(arr).reverse()).reverse();
+const neighborUniq = root.neighborUniq = (arr) => {
+    let out = [];
+    let cache = deepUniq(deepUniq(arr).reverse()).reverse();
+    cache.map(item => {
+        if (out.length === 0 || out.filter(internal => uniq(item, internal)).length > 0) {
+            out.push(item);
+        } else {
+            out.map(subItem => {
+                if (uniq(item, subItem)) {
+                    out.push(item);
+                }
+            });
+        } 
+    });
+    return out;
+};
 assert.deepEqual(neighborUniq([{a: 1}, {b: 2, c: 3}, {b: 2, c: 3}]), [{a: 1}, {b: 2, c: 3}]);
 assert.deepEqual(neighborUniq([{a: 1}, {b: 2, c: 3}, {b: 2, c: 3}, {d: 4}]), [{a: 1}, {b: 2, c: 3}, {d: 4}]);
 
